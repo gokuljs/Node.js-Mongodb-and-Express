ffdaeae32dde782bb88235cb9a34297ca24ac806
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-03-30 20:45:00 +0530

    demo app

--- node/grader.js
+++ node/grader.js
@@ -1,13 +1,14 @@
 function average(scores) {
 
-    var sum = 0;
-    scores.forEach(function(score) {
+    var sum = 0; // intailizing the sum
+    scores.forEach(function(score) { // using for each loop
+
         sum = sum + score;
     })
-    var avg = sum / scores.length;
-    round = Math.round(avg)
+    var avg = sum / scores.length; //taking average
+    round = Math.round(avg) // built in function to round of a number
     console.log(round);
 }
 
 var scores = [90, 98, 89, 100, 100, 86, 94];
-average(scores);
+average(scores); // calling the function
