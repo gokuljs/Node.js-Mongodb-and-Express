40bc4ec10da6f4ba9e1bda13772fafacbe2e3fd8
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-03-31 18:30:36 +0530

    getting started express app.js

--- first_express_app/app.js
+++ first_express_app/app.js
@@ -1,1 +1,21 @@
-console.log("getting started with express.js");
+var express = require("express"); //importing the express package
+var app = express(); // passing all rhe package functionality to the app
+// and for using any of the functionality app.functionality name
+
+
+// route this is part which painly deals with https in and out request 
+// "/" reprsents the home which here we going to get hi there 
+// "/bye" => you will get good bye 
+// "/dog" => "meow
+
+app.get("/", function(request, respond) {
+    res.send("hi there");
+
+});
+// when u make a get request to / or home page we have to see hi there  
+// to.get work there is something  called as app.listen(giving the port number,wat functionality should take place);
+
+
+app.listen(3000, function() {
+    console.log("serving on demo port 3000");
+})
