9fe2f5acf11f8a7d304366140f95141266f1efed
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-03 20:36:58 +0530

    ejs

--- EJS_demo/app.js
+++ EJS_demo/app.js
@@ -1,16 +1,20 @@
 var express = require("express");
 var app = express();
 
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", function(req, res) {
     //.res.render("dogs.ejs") // html file is passed
     // we trying build a dynamic type of web page 
 
     //res.send("<h1>welcome to home page</h1><h2>blah blah <        /h2>");
-    res.render("home.ejs");
+    res.render("home");
 });
 app.get("/fallinlovewith/:name", function(req, res) {
     var name = req.params.name;
-    res.render("love.ejs", { thingvar: name }); // accepting the variable and passing it as an object in love.ejs file
+    res.render("love", { thingvar: name }); // accepting the variable and passing it as an object in love.ejs file
     console.log(name);
 
     // .ejs stands for embedded javascript 
@@ -35,7 +39,7 @@ app.get("/posts", function(req, res) {
 
     ];
 
-    res.render("post.ejs", { posts: posts });
+    res.render("post", { posts: posts });
 });
 app.listen(3000, function() {
     console.log("server is listening");
