a2ceec616c8a24c1a5a4f9341d1d1c498287c6a8
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-04 07:32:38 +0530

    body-parser/posting/ejs

--- postrequestdemo/app.js
+++ postrequestdemo/app.js
@@ -1,7 +1,16 @@
 var express = require("express");
-var app = express();
+var app = express(); //intialising the express
 
-app.set("view engine", "ejs");
+var bodyparser = require("body-parser");
+// intialising the body parser to the variable 
+// syntax for telling the express to use body-parser
+app.use(bodyparser.urlencoded({ extended: true })); // telling express to use body parser
+
+
+app.set("view engine", "ejs"); //  creating a default file ejs
+var friends = ["dheeraj", "archith", "lal", "brj", "absk_x"]; // global declaration  of friends 
+// so all the routes can read it
+
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -10,18 +19,22 @@ app.get("/", function(req, res) {
 
 app.post("/addfriend", function(req, res) {
 
-    console.log(req.body);
+    var newfriend = req.body.newfriend // this is get data from the website bode 
+        // for this to work u have install a package body-parser
+        // tell the express to use body-parser 
+
+    friends.push(newfriend);
     //Node.js body parsing middleware.
 
     // req.body will contain the i nformation coming from the body
     //Note As req.body's shape is based on user-controlled input, all properties and values in this object are untrusted and should be validated before trusting. For example, req.body.foo.toString() may fail in multiple ways, for example the foo property may not be there or may not be a string, and toString may not be a function and instead a string or other user input.
-    res.send("you have reached the posting part");
+    res.redirect("/friends"); // it will redirect to the path web page 
 
 });
 
 app.get("/friends", function(req, res) {
-    var friend = ["dheeraj", "archith", "lal", "brj", "absk_x"];
-    res.render("friends", { friends: friend });
+
+    res.render("friends", { friends: friends });
 
 });
 
