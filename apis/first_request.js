6894c35f7e0698b041eea6ef0fcb6141eadb28a0
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-05 20:02:35 +0530

    getting started with weather_api

--- apis/first_request.js
+++ apis/first_request.js
@@ -5,15 +5,23 @@
 //     console.log('body:', body); // Print the HTML for the Google homepage.
 // });
 
+// var request = require("request");
+// request('https://www.google.com/', function(error, response, body) {
+//     if (error) {
+//         console.log("something went wrong");
+//         console.log("error");
+//     } else {
+//         if (response.statusCode == 200) {
+//             console.log("success");
+//             console.log(body);
+//         }
+//     }
+// });
+
 var request = require("request");
-request('https://www.google.com/', function(error, response, body) {
-    if (error) {
-        console.log("something went wrong");
-        console.log("error");
-    } else {
-        if (response.statusCode == 200) {
-            console.log("success");
-            console.log(body);
-        }
+request("", function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
     }
+
 });
