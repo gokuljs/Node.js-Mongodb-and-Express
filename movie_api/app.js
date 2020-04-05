54ee6b2c4c64ee4409cf8b4cb12a38000c2660be
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-06 05:29:50 +0530

    simple movie search app

--- movie_api/app.js
+++ movie_api/app.js
@@ -1,13 +1,21 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-app.set("view engiene", "ejs")
+app.set("view engine", "ejs");
 
+
+app.get("/", function(req, res) {
+    res.render("search")
+});
 app.get("/results", function(req, res) {
-    request("http://www.omdbapi.com/?s=californian&apikey=thewdb", function(error, response, body) {
+    console.log(req.query.search1);
+    var query = req.query.search1;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    console.log(url);
+    request(url, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            var result = JSON.parse(body);
-            res.send(result["Search"][0]["Title"]);
+            var data = JSON.parse(body);
+            res.render("results", { data: data });
         }
     })
 });
