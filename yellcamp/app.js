49c14caece81c388e77f0ec56b1b05beefb924de
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-06 06:25:23 +0530

    yellcamp basics

--- yellcamp/app.js
+++ yellcamp/app.js
@@ -10,8 +10,8 @@ app.get("/", function(req, res) {
 app.get("/campgrounds", function(req, res) {
     var campgrounds = [
         { name: "mountains", image: "https://lh3.googleusercontent.com/proxy/moNZl7sbmHTDLR4ox6WUmcyC--rNw-b7b5XJEQ9royBath4CV09JOjVMO5ghw-CPPrEDKg3tUPJQUlQZhOg50wtyCV6-k7zdRYF3dFrT9sexGMVuaWiYWc1PHoAAtj-XMHe9xpzodBrAf1YtB00-2ZMPasTlfG_aRg", },
-        { name: "art", image: "https://images.unsplashttps://lh3.googleusercontent.com/proxy/moNZl7sbmHTDLR4ox6WUmcyC--rNw-b7b5XJEQ9royBath4CV09JOjVMO5ghw-CPPrEDKg3tUPJQUlQZhOg50wtyCV6-k7zdRYF3dFrT9sexGMVuaWiYWc1PHoAAtj-XMHe9xpzodBrAf1YtB00-2ZMPasTlfG_aRgh.com/photo-1551913902-c92207136625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-        { name: "pattern", image: "https://images.unsplash.com/photo-1515387784663-e2e29a23f69e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80.jpg", },
+        { name: "art", image: "https://images.unsplash.com/photo-1551913902-c92207136625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
+        { name: "pattern", image: "https://images.unsplash.com/photo-1482160549825-59d1b23cb208?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
 
     ];
     res.render("campground", { campgrounds: campgrounds });
