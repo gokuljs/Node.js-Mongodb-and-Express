c553cbcceb92fd3cf24dbcb8cbbba9b324193825
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-14 02:11:36 +0530

    mongoose

--- database/cat.js
+++ database/cat.js
@@ -1,34 +1,12 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/cat_app");
+mongoose.connect("mongodb://localhost/data1");
 
-// first goind adiing all cats to database
-//then retrive all cats from database
 
-var catSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    temperament: String,
-}); //the process of declaring a pattern to the database
+var data = [{
+    name: "gokul js",
+    age: 100,
+    place: "banglore"
+}]
 
-// now the pattern which we declared should be converted into model
-// now this model should be passed to singular variable
-// we cann use that variable to find ,remove ,create ,update 
 
-var cat = mongoose.model("cat", catSchema);
-
-var george = new cat({
-    name: "gokul js ",
-    age: 12,
-    temperament: "grouc,xmxmhy",
-
-});
-
-george.save(function(err, cat) { // we are saving creating a call back function to check error is there or not
-
-    if (err) {
-        console.log("something went wrong");
-    } else {
-        console.log("saved");
-        console.log(cat);
-    }
-});
+var data1 = mongoose.model("data1", data)
