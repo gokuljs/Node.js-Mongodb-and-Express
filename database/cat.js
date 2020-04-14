9ccf1b792527dc450dc296d1cff7dd904270843b
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-14 06:41:33 +0530

    basics of mongoose.db

--- database/cat.js
+++ database/cat.js
@@ -1,12 +1,56 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/data1");
 
+var data = new mongoose.Schema({ // first creating a patterns 
 
-var data = [{
-    name: "gokul js",
-    age: 100,
-    place: "banglore"
-}]
+    name: String,
+    age: Number,
+    place: String
+});
+// compiling into a model and storing it in datas 
+
+var datas = mongoose.model("datas", data);
+
+// storing all the information in db
+// var george = new datas({
+//     name: "gokusdsdljs",
+//     age: 11,
+//     place: "banglore",
+
+// });
+
+// george.save(function(err, datas) {
+//     if (err) {
+//         console.log("error ")
+//     } else {
+//         console.log(datas)
+//     }
+
+// });
+
+datas.create({
+    name: "dheeraj",
+    age: 23,
+    place: "banglore",
+
+}, function(err, datas) {
+
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(datas);
+    }
 
 
-var data1 = mongoose.model("data1", data)
+
+});
+
+
+// // retrive all data from db and console.log each one 
+// datas.find({}, function(err, datas) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(datas);
+//     }
+// })
