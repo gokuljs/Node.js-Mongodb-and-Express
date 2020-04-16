501f77e773c4dc0a038b03c815e67ea66d078210
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-16 12:38:56 +0530

    yelpcamp v2

--- yellcamp/v2/app.js
+++ yellcamp/v2/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-//mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true }); // exact same syntax should be used everytime to remove the depriciated  warning 
+// exact same syntax should be used everytime to remove the depriciated  warning 
 //27017 represents the port number in which the mongodb is working on 
 mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -13,44 +13,20 @@ app.set("view engine", "ejs");
 var campgroundschema = new mongoose.Schema({
     name: String,
     img: String,
+    description: String,
 });
 
 var campground = mongoose.model("campground", campgroundschema);
 
-// campground.create({
-//     name: "Mountains",
-//     img: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-// }, function(err, campground) {
-//     if (err) {
-//         console.log("err");
-//     } else {
-//         // console.log(campground);
-//         console.log(done);
-//     }
-// });
 
 
 
 
 app.use(express.static("public"));
 
-// var campgrounds = [
-//     { name: "Mountains", image: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-//     { name: "Art", image: "https://images.unsplash.com/photo-1551913902-c92207136625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-//     { name: "Pattern", image: "https://images.unsplash.com/photo-1482160549825-59d1b23cb208?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
 
-
-//     { name: "Mountains", image: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-//     { name: "Art", image: "https://images.unsplash.com/photo-1551913902-c92207136625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-//     { name: "Pattern", image: "https://images.unsplash.com/photo-1482160549825-59d1b23cb208?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-//     { name: "Mountains", image: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-//     { name: "Art", image: "https://images.unsplash.com/photo-1551913902-c92207136625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-//     { name: "Pattern", image: "https://images.unsplash.com/photo-1482160549825-59d1b23cb208?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", },
-
-// ];
-// displays the home page 
 app.get("/", function(req, res) {
-    // res.send("landing page");
+
     res.render("landing")
 });
 
@@ -68,33 +44,9 @@ app.get("/campgrounds", function(req, res) {
 
     });
 
-    // res.render("campground", { campgrounds: campgrounds });
 });
 
 
-// app.post("/campgrounds", function(req, res) {
-
-//     // get data from form and add it to array 
-
-//     // var name = req.body.name;
-//     // var image = req.body.image;
-//     // console.log(name);
-//     // console.log(image);
-//     // var newcampground = {
-//     //         name: name,
-//     //         image: image,
-//     //     }
-//     //     // redirect back to campgrounds page 
-//     //     //res.send("you have enntered the post request part ");\
-//     //     // campgrounds.push(newcampground);
-//     // res.redirect("/campgrounds");
-//     res.render("you hit the post route")
-
-
-
-
-// });
-// creating a post request to add new campgrounds 
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
@@ -119,6 +71,10 @@ app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 });
 
+
+app.get("/campgrounds/:id", function(req, res) {
+    res.send("this will be show page one day ")
+});
 
 
 
--- yellcamp/v2/readme.md
+++ yellcamp/v2/readme.md
@@ -1,15 +1,19 @@
 # show page
 
-=============================================
-1 review the restful routes seen so far 
-2 add description to our campground model
-2 show db.collection.drop()
-4 and show route/template
-========================================
+============================================= \
+1 review the restful routes seen so far  \
+2 add description to our campground model \ 
+2 show db.collection.drop()  // use to remove all the collections at a time   \
+4 and show route/template \
+======================================== \
 
-name    url         verb       description
-==========================================
+name    url         verb       description \
+==========================================  \
 
-Index  /dogs        Get        display a list of all dog 
-new    /dogs/new    get        displays form to make a new dogs 
-create /dogs        post       and new dogs to db
+Index  /dogs        Get        display a list of all dog   \
+new    /dogs/new    get        displays form to make a new dogs   \
+create /dogs        post       and new dogs to db   \
+show   /dogs/:id    Get        shows information about one dog   \
+// it gives the dogs value based on the id value    
+
+===========================================
