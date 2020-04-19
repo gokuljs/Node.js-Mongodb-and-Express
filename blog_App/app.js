1e427f23dd483774b92a3e4641db736ddccf2f7f
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-19 19:44:27 +0530

    blog app

--- blog_App/app.js
+++ blog_App/app.js
@@ -2,10 +2,17 @@ var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+
+
+
+// app config 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost:27017/blog_app", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); // this tells us that where ever in ur app if _method then treat that request has put request 
+
 
 var blogschema = new mongoose.Schema({
     title: String,
@@ -55,11 +62,11 @@ app.post("/blogs", function(req, res) {
     })
 });
 
-
+// adding new post 
 app.get("/blogs/new", function(req, res) {
     res.render("new");
 });
-
+// show route 
 app.get("/blogs/:id", function(req, res) {
     blog.findById(req.params.id, function(err, foundblog) {
         if (err) {
@@ -70,6 +77,9 @@ app.get("/blogs/:id", function(req, res) {
     })
 });
 
+
+
+//edit route 
 app.get("/blogs/:id/edit", function(req, res) {
     console.log("id =" + req.params.id);
     blog.findById(req.params.id, function(err, foundblog) {
@@ -81,6 +91,18 @@ app.get("/blogs/:id/edit", function(req, res) {
 
     });
 
+});
+
+
+
+// normallly inside form tag the put request inside dosent works 
+// its acts as an get request so to able to make it work 
+// there is another function called method override 
+
+
+
+app.put("/blogs/:id", function(req, res) {
+    res.send("update route")
 });
 
 
