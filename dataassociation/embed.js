94ad9e28a55539d3a3f857926631b7932d83e088
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-20 21:04:40 +0530

    blog app

--- dataassociation/embed.js
+++ dataassociation/embed.js
@@ -7,36 +7,74 @@ mongoose.set('useFindAndModify', false);
 // user having email and name 
 
 
-var userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-});
-
-
-var user = mongoose.model("user", userSchema);
-
-
-// post will have title and content
-
-
 var postSchema = mongoose.Schema({
     title: String,
     content: String,
 
 });
+// post will have title and content
+
+
 
 var post = mongoose.model("post", postSchema);
 
+var userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    posts: [postSchema],
+});
+var user = mongoose.model("user", userSchema);
 
-var newuser = new user({
-    name: "gokul js ",
-    email: "jsgokul123@gmail.com"
-})
 
-newuser.save(function(err, user) {
+// var newuser = new user({
+//     name: "dheeraj",
+//     email: "dheerajc@gmail.com",
+//     posts: [{
+
+//         title: "gamer review",
+//         content: "welcome  gamer",
+//     }]
+
+// });
+
+
+// newuser.save(function(err, blog) {
+
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(blog);
+//     }
+
+// });
+
+
+
+
+
+// var newpost = new post({
+//     title: "welcome to new String ",
+//     content: "helllo getting started with embede data and data association  "
+// })
+
+
+user.findOne({ name: "dheeraj" }, function(err, user) {
     if (err) {
         console.log(err);
     } else {
-        console.log(user)
+        console.log(user);
+        user.posts.push({
+            title: "3 thing i really are",
+            content: "gamer, coder , changer",
+        });
+
+        user.save(function(err, user) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(user);
+            }
+        });
     }
-})
+
+});
