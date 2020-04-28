0a039c3d85cd59b25e9e389b1389f309b86cb349
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-28 09:06:41 +0530

    express

--- authentication/app.js
+++ authentication/app.js
@@ -4,6 +4,7 @@ var express = require("express"),
     bodyparser = require("body-parser"),
     passport = require("passport"),
     localStrategy = require("passport-local"),
+    user = require("./models/user"),
     passportLocalMongoose = require("passport-local-mongoose");
 
 
@@ -12,11 +13,21 @@ var express = require("express"),
 mongoose.connect("mongodb://localhost:27017/auth_demo_app", { useNewUrlParser: true, useUnifiedTopology: true });
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true }));
+//setting to use passport in the project
+//below two lines are required to run the passport in the project  
+app.use(passport.initialize()); // telling express to use passport
+app.use(passport.session())
 
 // app.use(express.static(__dirname + "/public"));
 // dirname refers to the directory name to the script was running 
 // console.log(__dirname);
 
+
+// adding express-seesion
+// using it and running it has a fucntion
+app.use("express-session" ({
+
+}))
 
 
 
