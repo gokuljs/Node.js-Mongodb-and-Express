a58417d158e7da27cf4a77d45c933ab005b904b4
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-06-11 16:13:36 +0530

    update campground

--- yellcamp1/routes/campgrounds.js
+++ yellcamp1/routes/campgrounds.js
@@ -99,25 +99,47 @@ router.get("/campgrounds/:id", function(req, res) {
 });
 
 // edit campground route
+// getting started with user autherization 
+
 router.get("/campgrounds/:id/edit", function(req, res) {
 
-    console.log(req.params.id);
-    campground.findById(req.params.id, function(err, foundcampground) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(foundcampground);
-            res.render("campgrounds/edit", { campground: foundcampground })
-        }
+    // logic for authorisation 
+    // if user is logged in 
+    //         does user own the campground
+    //         otherwise redirect 
+    //if not redirect
+    //
+    if (req.isAuthenticated()) {
+
+        // now checking if the used own the campground 
+        console.log(req.params.id);
+        campground.findById(req.params.id, function(err, foundcampground) {
+            if (err) {
+                console.log(err)
+            } else {
+                // does the user own the campground
+                console.log("checking user own the campground")
+                console.log("foundcampground = " + foundcampground.author.id);
+                console.log("userauthenticationid = " + req.user._id);
+                console.log(foundcampground);
+                res.render("campgrounds/edit", { campground: foundcampground })
+            }
 
 
-    });
+        });
+
+    } else {
+        console.log("u should log in tooo do that");
+        res.send("u should log in");
+    }
+
+
 
 
     // res.send("edit campground rout");
 
 });
-
+//update campground route 
 
 router.put("/campgrounds/:id", function(req, res) {
     console.log("put request")
@@ -133,7 +155,22 @@ router.put("/campgrounds/:id", function(req, res) {
     })
 
 });
-// update campground route 
+// delete campgroudn route
+
+router.delete("/campgrounds/:id", function(req, res) {
+    // res.send("you are going to delete camopground");
+    campground.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            console.log(err);
+            res.redirect("/");
+        } else {
+
+            res.redirect("/campgrounds");
+            console.log("delete route is working ")
+
+        }
+    })
+});
 
 
 function isLoggedIn(req, res, next) {
@@ -144,5 +181,8 @@ function isLoggedIn(req, res, next) {
     res.redirect("/login");
 
 }
+
+
+
 
 module.exports = router;
